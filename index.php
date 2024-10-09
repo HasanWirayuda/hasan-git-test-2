d313849a3a7d6a4e8e1435fb25ca5f3aedc73abc
Hasan Wirayuda <hasanwirayuda375@gmail.com>
2024-10-09 18:58:34 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi earum dignissimos ab molestiae voluptatibus alias consequatur, optio dolore facilis blanditiis porro accusantium cumque officiis voluptatem maiores suscipit sapiente veniam quas.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
