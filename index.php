e5a825b3a8b0347803e06a23a0efee870399a143
Hasan Wirayuda <hasanwirayuda375@gmail.com>
2024-10-09 18:48:06 +0700

    mengganti nama file dan manambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi earum dignissimos ab molestiae voluptatibus alias consequatur, optio dolore facilis blanditiis porro accusantium cumque officiis voluptatem maiores suscipit sapiente veniam quas.</p>
 </body>
+
 </html>
